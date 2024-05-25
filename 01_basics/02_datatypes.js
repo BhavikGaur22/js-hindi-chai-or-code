cab80fd85423f1d512a2c1cd4dc7c8ae3fc7ba13
BhavikGaur22 <102758356+BhavikGaur22@users.noreply.github.com>
2024-05-25 10:06:02 +0000

    02_datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,33 @@
+"use strict";   // treat all JS code as newer version
+
+//alert( 3 + 3)   // we can not use this in node js, only in browser
+
+console.log(3+
+    3) // code readability should be high 
+
+
+
+// JS Documentation tc39 & mdn
+
+let name = " bhavik"
+let age = 18
+let isLoggedIn = false
+
+
+/* Data Types
+
+Number => 2 to power 53
+bigint
+string => ""
+boolean => true/false
+null => standalone value, rpresentation of empty value. 
+undefined => 
+    symbol=> unique used in react mainly 
+
+Object 
+
+*/
+
+console.log(typeof age);
+
+
